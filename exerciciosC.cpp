600e0ee8bc341ac04cbd869d493af892045cb2ae
ggxaraujo <ggxaraujo@gmail.com>
2021-03-28 00:30:59 -0300

    Determine o volume de um cubo, exercicio 1

--- exerciciosC.cpp
+++ exerciciosC.cpp
@@ -2,8 +2,16 @@
 
 int main() { 
 
-    printf("programa");
-    printf("programassccxxxxcxcs");
+    float lado_cubo, volume; 
+
+    printf("Por favor o valor do lado do cubo:");
+    scanf("%f", &lado_cubo);
+
+    volume= lado_cubo * lado_cubo * lado_cubo;
+
+    printf("\nO volume do cubo e: %.1f \n", volume);
+
+
     
 
 }
