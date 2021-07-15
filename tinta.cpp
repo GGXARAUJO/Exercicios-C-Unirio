fcb226cb138c2cf2e60cd20c666afadfff58e1b2
ggxaraujo <ggxaraujo@gmail.com>
2021-07-15 02:56:04 -0300

    c

---
+++ tinta.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main (){
+    int tamanho_metros;
+    float valor_lata, litros_tinta;
+
+    printf("Insira o tamanho em metros quadrados da area: ");
+    scanf("%d", &tamanho_metros);
+
+    printf("Insira o valor da lata de tinta:  ");
+    scanf("%f", &valor_lata);
+    // processo
+
+    litros_tinta= 1 / 6; 
+
+}
