8b0cfe10c6d76ebd59bb0d619567fe2ec687f9c1
ggxaraujo <ggxaraujo@gmail.com>
2021-07-27 22:18:39 -0300

    while 1-10

---
+++ while.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int contador, n;
+
+printf("N:");
+scanf("%d", &n);
+
+    contador = 1;
+
+    while (contador <= n)
+    { 
+        printf( "%d ", contador);
+        contador = contador + 1;
+
+    }
+
+
+ }
