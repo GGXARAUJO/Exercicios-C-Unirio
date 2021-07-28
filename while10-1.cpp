21cc9aaa9a7bc434e73c8e3b18e5d091993a5f3d
ggxaraujo <ggxaraujo@gmail.com>
2021-07-27 22:22:46 -0300

    while inverso 10-1

---
+++ while10-1.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int contador, n;
+
+printf("N:");
+scanf("%d", &n);
+
+    contador = 10;
+
+    while (contador >= n)
+    { 
+        printf( "%d ", contador);
+        contador = contador - 1;
+
+    }
+
+
+ }
