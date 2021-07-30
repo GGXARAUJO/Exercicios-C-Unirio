b29ac6bad42fd11c93b2caa0e09acb2c7839f884
ggxaraujo <ggxaraujo@gmail.com>
2021-07-30 05:08:23 -0300

    numero par de 1 ate 50

---
+++ numerosparfor.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+
+    int numero;
+
+    
+
+    for (numero = 1; numero <= 50;numero++)
+    {
+        if( numero % 2 == 0) {
+            printf("%d\n", numero);
+
+        }
+        
+        
+    }
+}
